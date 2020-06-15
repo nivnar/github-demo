3905a5e5620fc79beec37d2174abf41fd74a05c1
Nivi <ninarva@microsoft.com>
2020-06-15 11:57:29 -0700

    script to sync cont-training with master branch

---
+++ syncgitbranch.ps1
@@ -0,0 +1,6 @@
+set-location C:\Users\ninarva\Documents\GitRepos\docs-help-pr
+Git pull checkout master
+Git pull origin master
+Git checkout contributor-training
+Git merge master
+Git push origin contributor-training
